ff0f80ec732e5a6f53907bb72da7140e24b49ec0
Travis Brown <travisdanielbrown@proton.me>
2025-01-13 14:38:34 -0500

    initial commit - starting IM project

---
+++ Travis_Brown_Inventory_Management/Form1.cs
@@ -0,0 +1,10 @@
+namespace Travis_Brown_Inventory_Management
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
