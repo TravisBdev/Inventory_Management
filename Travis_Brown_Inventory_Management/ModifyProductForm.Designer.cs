e2a201b2c0c4dc2e5d0a15a5b96e5be9d723ea67
Travis Brown <travisdanielbrown@proton.me>
2025-01-14 16:02:49 -0500

    Added product identification controls.

--- Travis_Brown_Inventory_Management/ModifyProductForm.Designer.cs
+++ Travis_Brown_Inventory_Management/ModifyProductForm.Designer.cs
@@ -36,6 +36,18 @@
             btnAddPart = new Button();
             btnCancelModifyProd = new Button();
             btnSaveModifyProd = new Button();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            textBox6 = new TextBox();
             ((System.ComponentModel.ISupportInitialize)dgvModProdAssociatedList).BeginInit();
             SuspendLayout();
             // 
@@ -104,7 +116,7 @@
             // 
             // btnDeletePart
             // 
-            btnDeletePart.Location = new Point(712, 244);
+            btnDeletePart.Location = new Point(713, 244);
             btnDeletePart.Name = "btnDeletePart";
             btnDeletePart.Size = new Size(55, 35);
             btnDeletePart.TabIndex = 5;
@@ -113,7 +125,7 @@
             // 
             // btnAddPart
             // 
-            btnAddPart.Location = new Point(648, 244);
+            btnAddPart.Location = new Point(649, 244);
             btnAddPart.Name = "btnAddPart";
             btnAddPart.Size = new Size(58, 35);
             btnAddPart.TabIndex = 6;
@@ -122,7 +134,7 @@
             // 
             // btnCancelModifyProd
             // 
-            btnCancelModifyProd.Location = new Point(760, 403);
+            btnCancelModifyProd.Location = new Point(761, 403);
             btnCancelModifyProd.Name = "btnCancelModifyProd";
             btnCancelModifyProd.Size = new Size(58, 35);
             btnCancelModifyProd.TabIndex = 7;
@@ -131,18 +143,126 @@
             // 
             // btnSaveModifyProd
             // 
-            btnSaveModifyProd.Location = new Point(696, 403);
+            btnSaveModifyProd.Location = new Point(697, 403);
             btnSaveModifyProd.Name = "btnSaveModifyProd";
             btnSaveModifyProd.Size = new Size(58, 35);
             btnSaveModifyProd.TabIndex = 8;
             btnSaveModifyProd.Text = "Save";
             btnSaveModifyProd.UseVisualStyleBackColor = true;
             // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(12, 142);
+            label3.Name = "label3";
+            label3.Size = new Size(18, 15);
+            label3.TabIndex = 9;
+            label3.Text = "ID";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(12, 183);
+            label4.Name = "label4";
+            label4.Size = new Size(39, 15);
+            label4.TabIndex = 10;
+            label4.Text = "Name";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(12, 223);
+            label5.Name = "label5";
+            label5.Size = new Size(57, 15);
+            label5.TabIndex = 11;
+            label5.Text = "Inventory";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(12, 264);
+            label6.Name = "label6";
+            label6.Size = new Size(33, 15);
+            label6.TabIndex = 12;
+            label6.Text = "Price";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(206, 300);
+            label7.Name = "label7";
+            label7.Size = new Size(28, 15);
+            label7.TabIndex = 13;
+            label7.Text = "Min";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(12, 300);
+            label8.Name = "label8";
+            label8.Size = new Size(30, 15);
+            label8.TabIndex = 14;
+            label8.Text = "Max";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(108, 139);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(154, 23);
+            textBox1.TabIndex = 15;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(108, 183);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(154, 23);
+            textBox2.TabIndex = 16;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(108, 220);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(154, 23);
+            textBox3.TabIndex = 17;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(108, 261);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(154, 23);
+            textBox4.TabIndex = 18;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(48, 297);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(78, 23);
+            textBox5.TabIndex = 19;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(240, 297);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(78, 23);
+            textBox6.TabIndex = 20;
+            // 
             // ModifyProductForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(830, 450);
+            Controls.Add(textBox6);
+            Controls.Add(textBox5);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
             Controls.Add(btnSaveModifyProd);
             Controls.Add(btnCancelModifyProd);
             Controls.Add(btnAddPart);
@@ -174,5 +294,17 @@
         private Button btnAddPart;
         private Button btnCancelModifyProd;
         private Button btnSaveModifyProd;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private TextBox textBox6;
     }
 }
